779d42ca6cec868d076e162cf85d4a6ec562c7c8
Darshil Thakore <darshilthakore@gmail.com>
2019-12-07 01:48:51 +0530

    realtime displayname availability check initialised

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -11,11 +11,25 @@ socketio = SocketIO(app)
 
 
 
-users = []
+users = ["darshil"]
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# route to check displayname availability
+@app.route("/checkdispname", methods=["POST"])
+def checkdispname():
+
+	# getting the displayname from the form
+	displayname = request.form.get("displayname")
+	if displayname in users:
+		prompt = "displayname not available"
+		return jsonify(prompt)
+	prompt = "username available"
+	return jsonify(prompt)
 
 
 @app.route("/channels")
--- static/index.js
+++ static/index.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	//checking the displayname availability
+	document.querySelector('[name="displayname"]').onkeyup = () => {
+		const displayname = document.querySelector('[name="displayname"]').value;
+		const request = new XMLHttpRequest();
+		request.open('POST', '/checkdispname');
+		request.onload = () => {
+			const response = JSON.parse(request.responseText);
+			response.forEach(run);
+
+
+		};
+
+		//send the displayname  along with the request
+		const data = new FormData();
+		data.append('displayname', displayname);
+		//send request
+
+		request.send(data);
+
+	};
+	function run(contents) {
+		document.querySelector('.validation').innerHTML = contents;
+	};
+
+
+	
+	// function checkdispname() {
+	// 	const displayname = document.querySelector('[name="displayname"]').value;
+		
+	// };
+
 
 
 
